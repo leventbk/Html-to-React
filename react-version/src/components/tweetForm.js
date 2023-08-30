e9d50c5c5d05b88f6f8ece0e18cec68a6e95abad
Leventbk <leventbkarakus@hotmail.com>
2023-08-30 16:00:08 -0400

    class to className

--- react-version/src/components/tweetForm.js
+++ react-version/src/components/tweetForm.js
@@ -2,18 +2,18 @@ import React from "react";
 
 function TweetForm() {
   return (
-    <main class="container">
+    <main className="container">
 
-    <section class="newtweet">
-      <form method="post" action="/tweets" class="newtweet__form">
-        <textarea class="form__textarea" name="text" placeholder="What are you humming about?"></textarea>
-        <input type="submit" value="Tweet" class="form__input"/>
-        <span class="form__counter">140</span>
+    <section className="newtweet">
+      <form method="post" action="/tweets" className="newtweet__form">
+        <textarea className="form__textarea" name="text" placeholder="What are you humming about?"></textarea>
+        <input type="submit" value="Tweet" className="form__input"/>
+        <span className="form__counter">140</span>
       </form>
     </section>
 
-    <div class="round-btn">
-      <i class="round-btn__icon fas fa-angle-double-up"></i>
+    <div className="round-btn">
+      <i className="round-btn__icon fas fa-angle-double-up"></i>
     </div>
   </main>
   );
