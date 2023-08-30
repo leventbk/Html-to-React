4bab25d6bf49536953b80f738e7e6ae37cbeb65d
Leventbk <leventbkarakus@hotmail.com>
2023-08-30 15:27:38 -0400

    tweetForm html

--- react-version/src/components/tweetForm.js
+++ react-version/src/components/tweetForm.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function TweetForm() {
-  return <h1>TweetForm component</h1>;
+  return (
+    <main class="container">
+
+    <section class="newtweet">
+      <form method="post" action="/tweets" class="newtweet__form">
+        <textarea class="form__textarea" name="text" placeholder="What are you humming about?"></textarea>
+        <input type="submit" value="Tweet" class="form__input"/>
+        <span class="form__counter">140</span>
+      </form>
+    </section>
+
+    <div class="round-btn">
+      <i class="round-btn__icon fas fa-angle-double-up"></i>
+    </div>
+  </main>
+  );
 }
 
 export default TweetForm;
