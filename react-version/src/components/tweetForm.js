d0ae56f3bc50bd3f0c417c44b72db78ecdc0f604
Leventbk <leventbkarakus@hotmail.com>
2023-08-30 16:26:51 -0400

    make vars

--- react-version/src/components/tweetForm.js
+++ react-version/src/components/tweetForm.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 function TweetForm() {
+  const placeholder = "What are you humming about?";
   return (
     <main className="container">
 
     <section className="newtweet">
       <form method="post" action="/tweets" className="newtweet__form">
-        <textarea className="form__textarea" name="text" placeholder="What are you humming about?"></textarea>
+        <textarea className="form__textarea" name="text" placeholder={placeholder}></textarea>
         <input type="submit" value="Tweet" className="form__input"/>
         <span className="form__counter">140</span>
       </form>
