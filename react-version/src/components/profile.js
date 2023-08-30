d1f10211c13e6ea07d338783b71c80c6efe38ef1
Leventbk <leventbkarakus@hotmail.com>
2023-08-30 16:25:22 -0400

    make vars

--- react-version/src/components/profile.js
+++ react-version/src/components/profile.js
@@ -1,14 +1,17 @@
 import React from "react";
 
 function Profile() {
+  const fistName = "Amy";
+  const lastName = "Mansell";
+  const profileImage = "./profile-hex.png";
   return (
     <aside>
     <div className="profile">
-      <img className="profile__image" src="./profile-hex.png"/>
+      <img className="profile__image" src={profileImage}/>
     </div>
     <br />
     <div className="profile__name">
-      <h2><span className="profile--bold">Amy</span> Mansell</h2>
+      <h2><span className="profile--bold">{fistName}</span> {lastName}</h2>
     </div>
   </aside>
   );
